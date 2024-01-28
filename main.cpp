16701726ab616fdbdb585cd6c333a2a12e2fa7ed
Luna <69774917+Lluna4@users.noreply.github.com>
2024-01-29 00:05:03 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,70 +9,22 @@
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
-#include <boost/multiprecision/cpp_int.hpp>
-#include <boost/uuid/uuid.hpp>
-#include <format>
+//include <format>
+#include <string.h>
+#include <thread>
+#include <uuid_v4/uuid_v4.h>
+#include <uuid_v4/endianness.h>
 
-enum req {STATUS = 1, LOGIN = 2};
+int stat = 0;
 
-static int	ft_intlen(int n)
+std::size_t ReadUleb128(const char* addr, unsigned long* ret) 
 {
-	int	ret;
-
-	ret = 1;
-	while (n >= 10)
-	{
-		ret++;
-		n = n / 10;
-	}
-	return (ret);
-}
-
-static std::string ft_make_ret(int n, int sign)
-{
-	int		len;
-
-	len = ft_intlen(n) + sign;
-	len--;
-    std::string ret(len + 1, '\0');
-	while (len >= 0)
-	{
-		ret[len] = (n % 10) + '0';
-		n = n / 10;
-		len--;
-	}
-	if (sign == 1)
-		ret[0] = '-';
-	return (ret);
-}
-
-std::string	ft_itoa(int n)
-{
-    std::string ret;
-	int		sign;
-
-	sign = 0;
-	if (n == -2147483648)
-	{
-		ret = "-2147483648";
-		return (ret);
-	}
-	if (n < 0)
-	{
-		n *= -1;
-		sign = 1;
-	}
-	return (ft_make_ret(n, sign));
-}
-
-
-std::size_t ReadUleb128(const char* addr, unsigned long* ret) {
   unsigned long result = 0;
   int shift = 0;
   std::size_t count = 0;
   
 
-  while (1) {
+  	while (1) {
     unsigned char byte = *reinterpret_cast<const unsigned char*>(addr);
     addr++;
     count++;
@@ -88,87 +40,128 @@ std::size_t ReadUleb128(const char* addr, unsigned long* ret) {
   return count;
 }
 
-char *read_string(char *addr, unsigned long len)
-{
-    char *ret = (char *)calloc(len + 1, sizeof(char));
-    int pos = 0;
-    while (pos < len)
-    {
-        ret[pos] = *addr;
-        pos++;
-        addr++;
-    }
-    return ret;
+std::size_t WriteUleb128(std::string &dest, unsigned long val) {
+  std::size_t count = 0;
+
+  do {
+    unsigned char byte = val & 0x7f;
+    val >>= 7;
+
+    if (val != 0)
+      byte |= 0x80;  // mark this byte to show that more bytes will follow
+
+    dest.push_back(byte);
+    count++;
+  } while (val != 0);
+
+  return count;
 }
 
-char *packet_login_success(boost::uuids::uuid uuid, char *uname)
+void send_pkt_succ(std::string uname, UUIDv4::UUID uuid, int sock)
 {
-    char *ret = (char*)calloc(100, sizeof(char));
-    char **iter;
-    int pos = 0;
-    //std::string ret;
-    ret[pos] = strlen(uname) + 19;
-    ret[pos + 1] = 0x02;
-    iter = &ret;
-    pos = pos + 2;
-    strlcpy(&ret[pos], (char *)uuid.data, strlen((char *)uuid.data));
-    pos = pos + 16;
-    ret[pos] = strlen(uname);
-    pos++;
-    strlcpy(&ret[pos], uname, strlen(uname) + 1);
-    return ret;
+	std::string pkt;
+	int pos = 0;
+	pos = (int)WriteUleb128(pkt, uname.length() + 16 + 3);
+	pkt.push_back(0x02);
+	pkt.append(uuid.bytes());
+    pkt.push_back((int)uname.length());
+	pkt.append(uname);
+	WriteUleb128(pkt, 0x00);
+	send(sock, pkt.c_str(), pkt.length(), 0);
+	std::cout <<  uname.length() + 16 + 3 << 4 << uuid.str() << uname.length() << uname << 0 << std::endl;
+	std::cout <<  uuid.bytes() << std::endl;
+	std::cout << pkt << std::endl;
+	stat = -1;
 }
 
-int main() 
+void manage_login(unsigned long len, char *pkt, std::string &uname, UUIDv4::UUID &uuid, int sock)
 {
-    int s = socket(AF_INET, SOCK_STREAM, 0);
-    unsigned long host_len = 0;
-    unsigned long prot = 0;
-    unsigned long port = 0;
-    boost::uuids::uuid uuid;
-    std::string packet;
+	unsigned long str = 0;
+	UUIDv4::UUIDGenerator<std::mt19937_64> uuidGenerator;
+	ReadUleb128(pkt, &str);
+	uname = pkt;
+	uname = uname.substr(1, str);
+	std::cout << pkt << std::endl;
+	std::cout << uname << std::endl;
+	pkt = pkt + str + 1;
+	uuid = uuidGenerator.getUUID();
+	std::cout << uuid.str() << std::endl;
+	send_pkt_succ(uname, uuid, sock);
+}
 
-    struct sockaddr_in addr = {
-        AF_INET,
-        htons(8081), // Use a specific port, e.g., 8080
-        0
-    };
+void manage_pkt(unsigned long len, char *pkt)
+{
+	unsigned long ver = 0;
+	unsigned long str = 0;
+	std::size_t prot_size = 0;
+	prot_size = ReadUleb128(pkt, &ver);
+	if (ver == 765)
+	{
+		pkt = pkt + prot_size;
+		ReadUleb128(pkt, &str);
+		pkt = pkt + 1;
+		//std::cout << pkt << std::endl;
+		pkt = pkt + (str + 2);
+		std::cout << (int)*pkt << std::endl;
+		stat = (int)*pkt;
+	}
+}
 
-    bind(s, (struct sockaddr*)&addr, sizeof(addr));
+void manage_sv(int sock)
+{
+	char buffer[1024] = {0};
+	char *ptr = buffer;
+	char *buff2;
+	unsigned long len = 0;
+	std::string uname;
+	UUIDv4::UUID uuid;
+	
+	while(1)
+	{
+		if (*ptr == '\0')
+		{
+			ptr = buffer;
+			recv(sock, buffer, 1023, 0);
+			len = (unsigned long)buffer[0];
+		}
+		else
+		{
+			len = (unsigned long)*ptr;
+		}
+		buff2 = (char *)calloc(len + 1, sizeof(char));
 
-    listen(s, 10);
-    char buffer[1024] = {0};
-    char *buff;
-    //char *uuid;
-    int client_fd = accept(s, 0, 0);
-    recv(client_fd, buffer, 100, 0);
-    //std::cout << buffer << std::endl;
-    char *a = buffer + 2;
-    ReadUleb128(a, &prot);
-    a += 2;
-    ReadUleb128(a, &host_len);
-    a++;
-    std::cout << prot << std::endl;
-    std::cout << host_len << std::endl;
-    buff = read_string(a, host_len);
-    std::cout << buff << std::endl;
-    a = a + host_len;
-    a = a + 2;
-    if ((int)*a == LOGIN)
-    {
-        a += 3;
-        ReadUleb128(a, &host_len);
-        a++;
-        std::cout << host_len << std::endl;
-        buff = read_string(a, host_len);
-        std::cout << buff << std::endl;
-        a = a + host_len;
-        std::memcpy(uuid.data, a, 16);
-        std::cout << a << std::endl;
-        char *pack = packet_login_success(uuid, buff);
-        std::cout << pack << std::endl;
-        send(client_fd, pack, strlen(pack), 0);
-        recv(client_fd, buffer, 100, 0);
-        std::cout << buffer << std::endl;
-    }
+		ptr = ptr + 2;
+		strncpy(buff2, ptr, len);
+		if (stat == 0)
+			manage_pkt(len, buff2);
+		else if (stat == 2)
+		{
+			manage_login(len, buff2, uname, uuid, sock);
+		}
+		free(buff2);
+		ptr = ptr + len;
+	}
+}
+
+int main()
+{
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in addr = {
+		AF_INET,
+		htons(25565),
+		0
+	};
+	if (bind(sockfd, (sockaddr *)&addr, sizeof(addr)) == -1)
+	{
+		std::cout << "Bind failed\n";
+		return -1;
+	}
+	while (1)
+	{
+		int client_fd = 0;
+		listen(sockfd, 32);
+		client_fd = accept(sockfd, nullptr, nullptr);
+		std::thread cli_th(manage_sv, client_fd);
+		cli_th.detach();
+	}
 }
